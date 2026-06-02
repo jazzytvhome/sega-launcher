843728d6dee33cc3d7ebfcb1719e62a08e07705a
justoneontop <jatinderhnk@gmail.com>
2026-06-02 11:40:21 +0200

    config: set dedicated Discord client id in launcher

--- launcher/SegaLauncher/AppConfig.cs
+++ launcher/SegaLauncher/AppConfig.cs
@@ -3,8 +3,8 @@ namespace SegaLauncher;
 public static class AppConfig
 {
     // PUBLIC values only. No secrets ever live in the launcher.
-    public const string DiscordClientId = "REPLACE_WITH_DISCORD_CLIENT_ID";
-    public const string VercelBaseUrl   = "https://sega-roads.vercel.app";
+    public const string DiscordClientId = "1511302943276793917";
+    public const string VercelBaseUrl   = "https://sega-roads.vercel.app"; // TODO: replace with your real Vercel prod URL after deploy.ps1
     public const int    LoopbackPort    = 51789;
 
     public static string RedirectUri => $"http://127.0.0.1:{LoopbackPort}/callback";
